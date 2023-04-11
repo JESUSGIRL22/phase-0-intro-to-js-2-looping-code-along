e394ddb8ca9c5e793aab04d4e3e7bc1377906ab0
techlover <e.rono@outlook.com>
2023-04-11 12:22:20 +0300

    Mission Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  const names = ["Charlie", "Ali", "Samip"];
+  const event = "birthday";
+  const messages = writeCards(names, event);
+  console.log(messages);
+  
+  countDown(5); // replace 5 with any other number as needed
+  
